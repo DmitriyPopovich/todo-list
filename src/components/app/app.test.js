4927e56594a84d76cb650ae549552cb9aa9432bc
DmitriyPopovich <dima.bas.85@gmail.com>
2022-05-02 22:09:20 +0300

    #4.7_user_app_total

--- src/components/app/app.test.js
+++ src/components/app/app.test.js
@@ -1,7 +1,7 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
-import App from "./app";
-import { renderWithRedux } from "../../tests/helpers/renderWithRedux";
+// import { render, screen } from "@testing-library/react";
+// import App from "./app";
+// import { renderWithRedux } from "../../tests/helpers/renderWithRedux";
 
 describe("Component App", () => {
   test("Main div exist on page", () => {
